126ab374b1ebec2beecc37667720a2b5b4ac22c2
fromthestone <md@lab51.org>
2019-06-17 17:17:45 +0800

    Remove unused DocType

--- mtmlogistic/mtm_logistic/doctype/mtm_shipment_way/mtm_shipment_way.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, MTM Technology Ltd and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('MTM Shipment Way', {
-	refresh: function(frm) {
-
-	}
-});
